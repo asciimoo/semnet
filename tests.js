e9784deb56080c707942e7cd62b6e7acae1657b4
asciimoo <asciimoo@gmail.com>
2012-09-03 20:50:28 +0200

    [enh] moar tests for queries

--- tests.js
+++ tests.js
@@ -1,7 +1,11 @@
 var semnet = require('./semnet.js');
+var util = require('util');
 
-function tests() {
-  var util = require('util');
+assert_inspected = function(a, b) {
+  return console.assert(util.inspect(a) === util.inspect(b));
+};
+
+function test() {
   var db = new semnet.Semnet();
   db.add('animal');
   db.add('mammal');
@@ -20,15 +24,18 @@ function tests() {
   console.assert(db.entities.dog.bigger.indexOf('ant') >= -1);
   var db2 = new semnet.Semnet();
   db2.import(db.export());
-  console.assert(util.inspect(db2.export()) == util.inspect(db.export()));
-  console.assert(util.inspect(db.q().filter('is', 'mammal').all()) == util.inspect(['lion']));
-  console.assert(util.inspect(db.q().filter('is', 'animal').all()) == util.inspect(['mammal', 'lion']));
-  console.assert(util.inspect(db.q().filter('contains', 'lion').all()) == util.inspect(['animal', 'mammal']));
+  assert_inspected(db2.export(), db.export());
+  assert_inspected(db.q().filter('is', 'mammal').all(), ['lion']);
+  assert_inspected(db.q().filter('is', 'animal').all(), ['mammal', 'lion']);
+  assert_inspected(db.q().filter('contains', 'lion').all(), ['animal', 'mammal']);
   db.fact('dog', 'is', 'mammal');
   db.fact('elephant', 'is', 'mammal');
   db.fact('cat', 'is', 'mammal');
+  var q = db.q();
+  assert_inspected(q.filter('is', 'animal').or(db.q().filter('smaller', 'lion')).all(), ['cat', 'ant', 'mammal', 'dog', 'lion', 'elephant']);
+  assert_inspected(db.q().all(), db.q().filter('is', 'animal').or(db.q()).all());
   return db;
 }
 
-db = tests();
+db = test();
 console.log('[+] Tests passed');
