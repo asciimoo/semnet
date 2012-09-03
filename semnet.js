326901242386188478c1b0a0970cf67471e163a9
asciimoo <asciimoo@gmail.com>
2012-09-03 18:41:07 +0200

    [mod][fix] node function separation ++ structural mods

--- semnet.js
+++ semnet.js
@@ -1,101 +1,105 @@
 
 function QuerySet(db) {
+  this.filter = function(relation, entity) {
+      if(this.query[relation]) this.query[relation].push(entity);
+      else this.query[relation] = [entity];
+      return this;
+  }
+
+  this.all = function() {
+    var ret = [];
+    for(ent in this.db.entities) {
+      var loop_count = 0, match_count = 0
+      for(relation in this.query) {
+        for(entity in this.query[relation]) {
+          loop_count++;
+          if(this.db.entities[ent][relation] && this.db.entities[ent][relation].indexOf(this.query[relation][entity]) != -1) {
+              match_count++;
+          }
+        }
+      }
+      if(match_count == loop_count) ret.push(ent);
+    }
+    return ret;
+  };
+
   this.query = {};
   this.db    = db;
+
   return this;
 }
 
-QuerySet.prototype.filter = function(relation, entity) {
-    if(this.query[relation]) this.query[relation].push(entity);
-    else this.query[relation] = [entity];
-    return this;
-}
+function Semnet() {
+  this.export = function() {
+    return {entities:  this.entities
+           ,relations: this.relations
+           };
+  }
 
-QuerySet.prototype.all = function() {
-  var ret = [];
-  for(ent in this.db.entities) {
-    var loop_count = 0, match_count = 0
-    for(relation in this.query) {
-      for(entity in this.query[relation]) {
-        loop_count++;
-        if(this.db.entities[ent][relation] && this.db.entities[ent][relation].indexOf(this.query[relation][entity]) != -1) {
-            match_count++;
-        }
+  this.import = function(json) {
+    if(json.entities) this.entities = json.entities;
+    if(json.relations) this.relations = json.relations;
+  }
+
+  this.add = function(name, options) {
+    if(!this.entities[name]) this.entities[name] = {};
+    if(!options) return;
+    this.relations[name] = {transitive : options.transitive?true:false
+                           ,opposite   : options.opposite?options.opposite:false
+                           };
+    if(options.opposite)
+      this.relations[options.opposite] = {transitive : options.transitive?true:false
+                                         ,opposite   : name
+                                         };
+  };
+
+  this.q = function() {
+    var query = new QuerySet(this);
+    return query;
+  }
+
+  this.fact = function(entity, relation_name, entitx) {
+    if(!this.entities[entity]) this.add_entity(entity);
+    if(!this.entities[entitx]) this.add_entity(entitx);
+    var relation = this.relations[relation_name];
+    if(!this.entities[entity][relation_name]) {
+      this.entities[entity][relation_name] = [entitx];
+    } else {
+      this.entities[entity][relation_name].push(entitx);
+    }
+    if(relation.opposite) {
+      if(!this.entities[entitx][relation.opposite]) {
+        this.entities[entitx][relation.opposite] = [entity];
+      } else {
+        this.entities[entitx][relation.opposite].push(entity);
       }
     }
-    if(match_count == loop_count) ret.push(ent);
-  }
-  return ret;
-};
+    if(relation.transitive) {
+      var relateds = this.entities[entitx][relation_name];
+      for(i in relateds) {
+        if(relateds[i] == entity || this.entities[entity][relation_name].indexOf(relateds[i]) != -1) continue;
+        this.fact(entity, relation_name, relateds[i]);
+      }
+      relateds = this.entities[entity][relation.opposite];
+      for(i in relateds) {
+        if(relateds[i] == entitx || this.entities[entitx][relation.opposite].indexOf(relateds[i]) != -1) continue;
+        this.fact(entitx, relation.opposite, relateds[i]);
+      }
+    }
+  };
 
-function Semnet() {
   this.entities  = {};
   this.relations = {};
   this.add('is', {opposite: 'contains', transitive: true});
+
   return this;
 }
 
-Semnet.prototype.export = function() {
-  return {entities:  this.entities
-         ,relations: this.relations
-         };
-}
 
-Semnet.prototype.import = function(json) {
-  if(json.entities) this.entities = json.entities;
-  if(json.relations) this.relations = json.relations;
-}
-
-Semnet.prototype.add = function(name, options) {
-  if(!this.entities[name]) this.entities[name] = {};
-  if(!options) return;
-  this.relations[name] = {transitive : options.transitive?true:false
-                         ,opposite   : options.opposite?options.opposite:false
-                         };
-  if(options.opposite)
-    this.relations[options.opposite] = {transitive : options.transitive?true:false
-                                       ,opposite   : name
-                                       };
-};
-
-Semnet.prototype.q = function() {
-  var query = new QuerySet(this);
-  return query;
-}
-
-Semnet.prototype.fact = function(entity, relation_name, entitx) {
-  if(!this.entities[entity]) this.add_entity(entity);
-  if(!this.entities[entitx]) this.add_entity(entitx);
-  var relation = this.relations[relation_name];
-  if(!this.entities[entity][relation_name]) {
-    this.entities[entity][relation_name] = [entitx];
-  } else {
-    this.entities[entity][relation_name].push(entitx);
-  }
-  if(relation.opposite) {
-    if(!this.entities[entitx][relation.opposite]) {
-      this.entities[entitx][relation.opposite] = [entity];
-    } else {
-      this.entities[entitx][relation.opposite].push(entity);
-    }
-  }
-  if(relation.transitive) {
-    var relateds = this.entities[entitx][relation_name];
-    for(i in relateds) {
-      if(relateds[i] == entity || this.entities[entity][relation_name].indexOf(relateds[i]) != -1) continue;
-      this.fact(entity, relation_name, relateds[i]);
-    }
-    relateds = this.entities[entity][relation.opposite];
-    for(i in relateds) {
-      if(relateds[i] == entitx || this.entities[entitx][relation.opposite].indexOf(relateds[i]) != -1) continue;
-      this.fact(entitx, relation.opposite, relateds[i]);
-    }
-  }
-};
-
-var db = new Semnet();
-
-if(this.require != undefined) {
-  var repl = require('repl');
-  repl.start().context.db = db;
+if(require != undefined) {
+  module.exports.Semnet = Semnet;
+  module.exports.QuerySet = QuerySet;
+  //var db = new Semnet();
+  //var repl = require('repl');
+  //repl.start().context.db = db;
 }
